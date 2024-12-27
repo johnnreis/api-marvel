ec151c6912ab1163fbb7e9b273016e5666c921de
John Reis <jhonatareisy7@gmail.com>
2024-12-27 12:53:32 -0300

    nav: configuring container of navigator to the project

--- app/src/main/java/com/example/api_marvel/presentation/MainActivity.kt
+++ app/src/main/java/com/example/api_marvel/presentation/MainActivity.kt
@@ -3,9 +3,17 @@ package com.example.api_marvel.presentation
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import com.example.api_marvel.R
 import com.example.api_marvel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +21,14 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_container) as NavHostFragment
+
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(
+            setOf()
+        )
 
     }
 }
