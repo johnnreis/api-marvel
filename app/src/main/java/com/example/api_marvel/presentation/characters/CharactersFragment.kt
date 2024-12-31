834851530299260b645a9acf3982769be489aee8
John Reis <jhonatareisy7@gmail.com>
2024-12-31 00:19:56 -0300

    style: implements fragments for characters with mock data #20

--- app/src/main/java/com/example/api_marvel/presentation/characters/CharactersFragment.kt
+++ app/src/main/java/com/example/api_marvel/presentation/characters/CharactersFragment.kt
@@ -6,15 +6,63 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.api_marvel.R
+import com.example.api_marvel.databinding.FragmentCharactersBinding
+import com.example.api_marvel.presentation.characters.adapter.CharactersAdapter
+import com.example.core.domain.model.Character
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class CharactersFragment : Fragment() {
 
+    private var _binding : FragmentCharactersBinding? = null
+    private val binding get() = _binding!!
+
+    private val charactersAdapter = CharactersAdapter()
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_characters, container, false)
+    ): View {
+        _binding = FragmentCharactersBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initCharactersAdapter()
+
+        charactersAdapter.submitList(
+            listOf(
+                Character("3D-Man", "https://cinepop.com.br/wp-content/uploads/" +
+                        "2019/03/capitamarvel_27-1280x720-696x392.jpg"),
+                Character("3D-Man", "https://cinepop.com.br/wp-content/uploads/" +
+                        "2019/03/capitamarvel_27-1280x720-696x392.jpg"),
+                Character("3D-Man", "https://cinepop.com.br/wp-content/uploads/" +
+                        "2019/03/capitamarvel_27-1280x720-696x392.jpg"),
+                Character("3D-Man", "https://cinepop.com.br/wp-content/uploads/" +
+                        "2019/03/capitamarvel_27-1280x720-696x392.jpg"),
+                Character("3D-Man", "https://cinepop.com.br/wp-content/uploads/" +
+                        "2019/03/capitamarvel_27-1280x720-696x392.jpg"),
+                Character("3D-Man", "https://cinepop.com.br/wp-content/uploads/" +
+                        "2019/03/capitamarvel_27-1280x720-696x392.jpg"),
+            )
+        )
+
+    }
+
+    private fun initCharactersAdapter() {
+        binding.recyclerCharacters.run {
+            setHasFixedSize(true)
+            adapter = charactersAdapter
+        }
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
