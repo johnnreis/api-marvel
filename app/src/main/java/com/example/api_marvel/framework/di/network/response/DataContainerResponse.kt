59416b88213ab9b7d2cc256208e35f995402eef1
John Reis <jhonatareisy7@gmail.com>
2025-01-01 16:26:58 -0300

    feat: implement the PagingSource of Characters #28

--- app/src/main/java/com/example/api_marvel/framework/di/network/response/DataContainerResponse.kt
+++ app/src/main/java/com/example/api_marvel/framework/di/network/response/DataContainerResponse.kt
@@ -1,5 +1,7 @@
 package com.example.api_marvel.framework.di.network.response
 
 data class DataContainerResponse(
+    val offset: Int,
+    val total: Int,
     val results: List<CharacterResponse>
 )
